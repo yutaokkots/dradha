abcaf480ea8ca0a0c9728f4d623764975134db07
yutaokkots <yuta.okkotsu@gmail.com>
2023-04-16 13:52:41 -0600

    Able to see time progress through circular progress bar

--- src/components/timerComponents/Clock/TimerComp.jsx
+++ src/components/timerComponents/Clock/TimerComp.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
 import { useTimer } from 'react-timer-hook';
-import { useState } from 'react'
 import ProgressBar from 'react-customizable-progressbar'
 
 
@@ -11,13 +10,22 @@ export default function TimerComp({inputTime}) {
         start, pause, resume, restart,
     } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
 
-    const [timerToggle, setTimerToggle] = useState(1)
+    const [second, setSecond] = useState(0)
+    const [totalSecond, setTotalSecond] = useState(0)
+
+    useEffect(()=>{
+        setSecond(convertSeconds(seconds,minutes,hours,days))
+        setTotalSecond(convertSeconds(0, inputTime))
+        console.log(second)
+        console.log(totalSecond)
+    }, [seconds, minutes, hours, days])
 
     function twoDigits(singleDigit){
         if (singleDigit.toString().length === 1) return `0${singleDigit}`
         else return singleDigit
     }
 
+    // sets the user input, in min, to the time that the timer runs
     function setTimerComp(inputTime){
         console.log(inputTime*60)
         let seconds = inputTime * 60
@@ -26,18 +34,27 @@ export default function TimerComp({inputTime}) {
         restart(time)
     }
 
+    //   progress bar
+    // converting input time  information to seconds so it displays in the progress bar
+    function convertSeconds(seconds = 0, minutes = 0, hours = 0, days = 0){
+        return seconds + minutes * 60 + hours * 60 * 60 + days * 24 * 60 * 60
+    }
+
+    // convertSeconds(seconds, minutes, hours)
+    // convertSeconds(0, inputTime)
     return (
         <>
-
-             <ProgressBar
-                radius={50}
-                progress={() => convertSeconds(seconds, minutes, hours)}
-                steps={300}
-                />
-
             <div style={{textAlign: 'center'}}>
             <h1>Timer</h1>
-            <div style={{fontSize: '100px'}}>
+            <div style={{fontSize: '100px', display:'flex', justifyContent: 'center'}}>
+                <ProgressBar
+                    radius={55}
+                    transition={'1s ease'}
+                    counterClockwise={true}
+                    inverse={true}
+                    progress={second}
+                    steps={totalSecond}
+                    />
                 {(hours) ? <span>{twoDigits(hours)}</span>: <span></span>}<span>{twoDigits(minutes)}</span>:<span>{twoDigits(seconds)}</span>
             </div>
             <p>{isRunning ? 'Running' : 'Not running'}</p>
