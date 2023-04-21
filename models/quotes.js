d66404dddb4beeba52fa56f44ebbeafb080f87b4
yutaokkots <yuta.okkotsu@gmail.com>
2023-04-20 20:15:34 -0600

    set up seed file for seeding data

---
+++ models/quotes.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const quotesSchema = new Schema({
+    quote:{
+        type: String,
+        required: true
+    },
+    source: {
+        type: Date,
+        required: false
+    },
+    },{
+    timestamps: true,
+    toJSON: { virtuals: true }
+});
+  
+
+  module.exports = mongoose.model('Quotes', quotesSchema);
