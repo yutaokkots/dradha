5a4d60ef9c65abdb4064825bddd176629ab4a9f0
yutaokkots <yuta.okkotsu@gmail.com>
2023-04-23 03:04:31 -0600

    ran build and updated code

--- src/components/Inspiration/Inspiration.jsx
+++ src/components/Inspiration/Inspiration.jsx
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 
 export default function Inspiration({quote, getNewQuote}) {
-
     function handleClick(){
         getNewQuote()
     }
